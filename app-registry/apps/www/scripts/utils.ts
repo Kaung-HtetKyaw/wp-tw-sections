3580ab1b5d1b2a08c857364287ecba57b33d2190
KaungHtet-Kyaw <kaunghtetkyaw02749@gmail.com>
2024-06-04 10:14:32 +0700

    add build command

--- app-registry/apps/www/scripts/utils.ts
+++ app-registry/apps/www/scripts/utils.ts
@@ -1,5 +1,6 @@
 export const removeDuplicates = <T>(items: T[]): T[] => {
   return items
     .filter((item, index, self) => self.findIndex((c) => item === c) === index)
+
     .filter(Boolean);
 };
